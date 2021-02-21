46f2956b1776c9163f3f10aa101be8ab1715cda0
tolumide-ng <tolumideshopein@gmail.com>
2021-02-21 15:14:23 +0100

    abstract players movement into move_racket

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,66 @@
-use event::run;
+use ggez::input::keyboard::{self, KeyCode};
 use ggez::{self, event, graphics, nalgebra as na, Context, ContextBuilder, GameResult};
 use graphics::{DrawParam, Drawable};
-use na::Point2;
 
 const RACKET_HEIGHT: f32 = 100.0;
 const RACKET_WIDTH: f32 = 20.0;
 const RAKCET_HEIGHT_HALF: f32 = RACKET_HEIGHT * 0.5;
 const RACKET_WIDTH_HALF: f32 = RACKET_WIDTH * 0.5;
+const BALL_SIZE: f32 = 30.0;
+const BALL_SIZE_HALF: f32 = BALL_SIZE * 0.5;
+const PLAYER_SPEED: f32 = 600.0;
+
+fn clamp(value: &mut f32, low: f32, high: f32) {
+    if *value < low {
+        *value = low;
+    } else if *value > high {
+        *value = high
+    }
+}
 
 struct MainState {
     player_1_pos: na::Point2<f32>,
     player_2_pos: na::Point2<f32>,
+    ball_pos: na::Point2<f32>,
+}
+
+fn move_racket(pos: &mut na::Point2<f32>, keycode: KeyCode, y_dir: f32, ctx: &mut Context) {
+    let screen_h = graphics::drawable_size(ctx).1;
+    let dt = ggez::timer::delta(ctx).as_secs_f32();
+
+    if keyboard::is_key_pressed(ctx, keycode) {
+        pos.y += y_dir * PLAYER_SPEED * dt;
+    }
+
+    clamp(
+        &mut pos.y,
+        RAKCET_HEIGHT_HALF,
+        screen_h - RAKCET_HEIGHT_HALF,
+    );
 }
 
 impl MainState {
     fn new(ctx: &mut Context) -> Self {
         let (screen_w, screen_h) = graphics::drawable_size(ctx);
-        let (screen_w_h, screen_h_half) = (screen_w * 0.5, screen_h * 0.5);
+        let (screen_w_half, screen_h_half) = (screen_w * 0.5, screen_h * 0.5);
 
         MainState {
             player_1_pos: na::Point2::new(RACKET_WIDTH_HALF, screen_h_half),
             player_2_pos: na::Point2::new(screen_w - RACKET_WIDTH_HALF, screen_h_half),
+            ball_pos: na::Point2::new(screen_w_half, screen_h_half),
         }
     }
 }
 
 impl event::EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
+        let dt = ggez::timer::delta(ctx).as_secs_f32();
+
+        move_racket(&mut self.player_1_pos, KeyCode::W, -1.0, ctx);
+        move_racket(&mut self.player_1_pos, KeyCode::S, 1.0, ctx);
+        move_racket(&mut self.player_2_pos, KeyCode::Up, -1.0, ctx);
+        move_racket(&mut self.player_2_pos, KeyCode::Down, 1.0, ctx);
+
         Ok(())
     }
 
@@ -40,12 +74,19 @@ impl event::EventHandler for MainState {
             RACKET_HEIGHT,
         );
 
-        // let color = [0.0, 0.0, 1.0, 1.0].into();
-
         let racket_mesh = graphics::Mesh::new_rectangle(
             ctx,
             graphics::DrawMode::fill(),
             racket_rect,
+            graphics::WHITE,
+        )?;
+
+        let ball_rect = graphics::Rect::new(-BALL_SIZE_HALF, -BALL_SIZE_HALF, BALL_SIZE, BALL_SIZE);
+
+        let ball_mesh = graphics::Mesh::new_rectangle(
+            ctx,
+            graphics::DrawMode::fill(),
+            ball_rect,
             graphics::WHITE,
         )?;
 
@@ -56,6 +97,9 @@ impl event::EventHandler for MainState {
 
         draw_param.dest = self.player_2_pos.into();
         graphics::draw(ctx, &racket_mesh, draw_param)?;
+
+        draw_param.dest = self.ball_pos.into();
+        graphics::draw(ctx, &ball_mesh, draw_param)?;
 
         graphics::present(ctx)?;
 
